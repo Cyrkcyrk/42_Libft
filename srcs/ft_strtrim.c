f0381f61c643516c54d29acce6750197b438af81
Cyrille KASYC <cyrkcyrk99@gmail.com>
2021-01-05 00:43:17 +0100

    strtrim fixed

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ckasyc <ckasyc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 22:18:47 by ckasyc            #+#    #+#             */
-/*   Updated: 2020/12/29 16:10:00 by ckasyc           ###   ########.fr       */
+/*   Updated: 2021/01/05 00:42:21 by ckasyc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		len;
 
-	if (!s1 || !set )//|| !(ret = malloc((ft_strlen(s1) + 1) * sizeof(char))))
+	if (!s1 || !set)
 		return (NULL);
 	start = 0;
 	while (ft_strchr(set, s1[start]) && s1[start] != '\0')
 		start++;
 	if (s1[start] == '\0')
-		return (NULL);
-	//start = (char *)&(s1[i]);
+		return (ft_strdup(""));
 	i = -1;
 	while (s1[++i + start] != '\0')
 		if (!ft_strchr(set, s1[i + start]))
