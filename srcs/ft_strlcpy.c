f7125eb1d9420d36ec8b91c549901df9c07cb3e2
Cyrille KASYC <cyrkcyrk99@gmail.com>
2021-01-04 12:47:00 +0100

    Edit strlcpy

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ckasyc <ckasyc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 12:45:07 by ckasyc            #+#    #+#             */
-/*   Updated: 2020/12/28 15:50:27 by ckasyc           ###   ########.fr       */
+/*   Updated: 2021/01/04 12:45:59 by ckasyc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int i;
 	unsigned int len_src;
 
+	if (size == 0)
+		return (0);
 	i = 0;
 	len_src = ft_strlen(src);
 	while (size > 0 && i < size - 1 && i < len_src)
